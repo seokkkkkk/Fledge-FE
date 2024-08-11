ca044bdb11855905dca9033581740f0c36ec767b
seokkkkkk <tjr6613@seoultech.ac.kr>
2024-08-11 03:41:41 +0000

    fix: 자청년 인증시 인증상태 바로 반영되게 수정

--- src/components/MyPage/CanaryModal.tsx
+++ src/components/MyPage/CanaryModal.tsx
@@ -8,299 +8,295 @@ import PostalCode from "./PostalCode";
 import useAuthStore from "../../storage/useAuthStore";
 import { getPresignedUrl, uploadImageToS3 } from "../../apis/file-upload";
 import { postCanaryApply } from "../../apis/canary";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export interface postCode {
-    address: string;
-    zonecode: number | string;
-    detailAddress?: string;
+  address: string;
+  zonecode: number | string;
+  detailAddress?: string;
 }
 
 // prop 타입 정의
 interface CanaryModalProps {
-    onClick: () => void;
+  onClick: () => void;
 }
 
 const CanaryModal = ({ onClick }: CanaryModalProps) => {
-    const { userData } = useAuthStore.getState();
-    const [success, setSuccess] = useState(false);
-    const [image, setImage] = useState<File | null>(null);
-    const fileInputRef = useRef<HTMLInputElement>(null);
-    const accesstoken = useAuthStore((state) => state.accessToken)!;
-    const [birthData, setBirthData] = useState({
-        year: "2000",
-        month: "1",
-        day: "1",
+  const { userData } = useAuthStore.getState();
+  const [success, setSuccess] = useState(false);
+  const [image, setImage] = useState<File | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const accesstoken = useAuthStore((state) => state.accessToken)!;
+  const [birthData, setBirthData] = useState({
+    year: "2000",
+    month: "1",
+    day: "1",
+  });
+  const [applyData, setApplyData] = useState({
+    userId: userData.id || 0,
+    name: userData.nickname || "",
+    phone: "",
+    birth: "",
+    gender: true,
+    address: "",
+    detailAddress: "",
+    zip: "" as number | string,
+    certificateFilePath: "",
+    latitude: 0,
+    longitude: 0,
+  });
+  const handleApplyData = (value: any, field: any) => {
+    let formattedValue = value;
+
+    // 필드에 따라 포맷팅 처리
+    if (field === "phone") {
+      formattedValue = formatPhoneNumber(value);
+    }
+
+    setApplyData({
+      ...applyData,
+      [field]: formattedValue,
     });
-    const [applyData, setApplyData] = useState({
-        userId: userData.id || 0,
-        name: userData.nickname || "",
-        phone: "",
-        birth: "",
-        gender: true,
-        address: "",
-        detailAddress: "",
-        zip: "" as number | string,
-        certificateFilePath: "",
-        latitude: 0,
-        longitude: 0,
+  };
+
+  const handleBirthData = useCallback(
+    (data: any, key: string) => {
+      setBirthData({ ...birthData, [key]: data });
+    },
+    [birthData]
+  );
+
+  const handleAddressChange = (data: postCode) => {
+    setApplyData({
+      ...applyData,
+      address: data.address,
+      detailAddress: data.detailAddress!,
+      zip: data.zonecode,
     });
-    const handleApplyData = (value: any, field: any) => {
-        let formattedValue = value;
+  };
 
-        // 필드에 따라 포맷팅 처리
-        if (field === "phone") {
-            formattedValue = formatPhoneNumber(value);
-        }
+  const handleImageChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files ? e.target.files[0] : null;
+      if (file) {
+        setImage(file);
+      }
+    },
+    []
+  );
 
-        setApplyData({
-            ...applyData,
-            [field]: formattedValue,
-        });
-    };
+  const formatPhoneNumber = (value: any) => {
+    // 숫자만 남기기
+    const numbersOnly = value.replace(/\D/g, "");
 
-    const handleBirthData = useCallback(
-        (data: any, key: string) => {
-            setBirthData({ ...birthData, [key]: data });
-        },
-        [birthData]
-    );
+    // 3자리, 4자리, 4자리로 나누어 하이픈 추가
+    let formattedNumber = numbersOnly;
+    if (numbersOnly.length > 3 && numbersOnly.length <= 7) {
+      formattedNumber = `${numbersOnly.slice(0, 3)}-${numbersOnly.slice(3)}`;
+    } else if (numbersOnly.length > 7) {
+      formattedNumber = `${numbersOnly.slice(0, 3)}-${numbersOnly.slice(
+        3,
+        7
+      )}-${numbersOnly.slice(7, 11)}`;
+    }
 
-    const handleAddressChange = (data: postCode) => {
-        setApplyData({
-            ...applyData,
-            address: data.address,
-            detailAddress: data.detailAddress!,
-            zip: data.zonecode,
-        });
-    };
+    return formattedNumber;
+  };
 
-    const handleImageChange = useCallback(
-        (e: React.ChangeEvent<HTMLInputElement>) => {
-            const file = e.target.files ? e.target.files[0] : null;
-            if (file) {
-                setImage(file);
-            }
-        },
-        []
-    );
+  const convertToISO = (year: number, month: number, day: number): string => {
+    const date = new Date(year, month - 1, day);
+    return date.toISOString();
+  };
 
-    const formatPhoneNumber = (value: any) => {
-        // 숫자만 남기기
-        const numbersOnly = value.replace(/\D/g, "");
+  // QueryClient 가져오기
+  const queryClient = useQueryClient();
 
-        // 3자리, 4자리, 4자리로 나누어 하이픈 추가
-        let formattedNumber = numbersOnly;
-        if (numbersOnly.length > 3 && numbersOnly.length <= 7) {
-            formattedNumber = `${numbersOnly.slice(0, 3)}-${numbersOnly.slice(
-                3
-            )}`;
-        } else if (numbersOnly.length > 7) {
-            formattedNumber = `${numbersOnly.slice(0, 3)}-${numbersOnly.slice(
-                3,
-                7
-            )}-${numbersOnly.slice(7, 11)}`;
-        }
+  // useMutation을 사용하여 데이터 제출 및 applyStatus를 갱신
+  const mutation = useMutation({
+    mutationFn: async () => {
+      const uploadApplyData = { ...applyData };
+      if (image) {
+        const presignedData = await getPresignedUrl(
+          "canary-certificate",
+          image.name,
+          accesstoken
+        );
+        const presignedUrl = presignedData.data.url;
+        const filePath = presignedData.data.filePath;
+        await uploadImageToS3(image, presignedUrl);
+        uploadApplyData.certificateFilePath = filePath;
+      }
+      if (applyData.userId) {
+        const { year, month, day } = birthData;
+        const birth = convertToISO(+year, +month, +day);
+        uploadApplyData.birth = birth;
+        return postCanaryApply(uploadApplyData, accesstoken);
+      }
+    },
+    onSuccess: () => {
+      // postCanaryApply가 성공적으로 완료되었을 때 applyStatus 다시 가져오기
+      queryClient.invalidateQueries({
+        queryKey: ["getCanaryStatus", userData.id, accesstoken],
+      });
+      setSuccess(true);
+    },
+    onError: () => {
+      alert("제출에 실패했습니다.");
+    },
+  });
 
-        return formattedNumber;
-    };
+  // 제출 버튼 클릭 시 mutation 실행
+  const onSubmit = useCallback(() => {
+    mutation.mutate();
+  }, [mutation]);
 
-    const convertToISO = (year: number, month: number, day: number): string => {
-        const date = new Date(year, month - 1, day);
-        return date.toISOString();
-    };
+  console.log(applyData);
 
-    const onSubmit = useCallback(async () => {
-        const uploadApplyData = { ...applyData };
-        if (image) {
-            const presignedData = await getPresignedUrl(
-                "canary-certificate",
-                image.name,
-                accesstoken
-            );
-            const presignedUrl = presignedData.data.url;
-            const filePath = presignedData.data.filePath;
-            await uploadImageToS3(image, presignedUrl);
-            uploadApplyData.certificateFilePath = filePath;
-        }
-        if (applyData.userId) {
-            const { year, month, day } = birthData;
-            const birth = convertToISO(+year, +month, +day);
-            uploadApplyData.birth = birth;
-            const res = await postCanaryApply(uploadApplyData, accesstoken);
-            if (res ? res.success : false) {
-                setSuccess(true);
-            } else {
-                alert("제출에 실패했습니다.");
-            }
-        }
-    }, [accesstoken, applyData, image, birthData]);
-
-    console.log(applyData);
-
-    return (
-        <CanaryWrapper
-            onClick={() => {
-                onClick();
-            }}
+  return (
+    <CanaryWrapper
+      onClick={() => {
+        onClick();
+      }}
+    >
+      {!success ? (
+        <Canary
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
         >
-            {!success ? (
-                <Canary
-                    onClick={(e) => {
-                        e.stopPropagation();
-                    }}
-                >
-                    <span className="title-text">자립준비청년 인증하기</span>
-                    <div className="input-section main-text">
-                        <div className="sub-section">
-                            <div className="w-[662.79px]">
-                                <span className="sub-text">
-                                    보호종료확인서 업로드
-                                </span>
-                                <ImageInput>
-                                    <div className="input-field">
-                                        <Input
-                                            type="file"
-                                            onChange={handleImageChange}
-                                            style={{ display: "none" }} // Hide the file input
-                                            ref={fileInputRef} // Use a ref to interact with this input
-                                        />
-                                        <span>{image?.name || ""}</span>
-                                    </div>
-                                    <Button
-                                        title="업로드"
-                                        mainColor
-                                        small
-                                        onClick={() =>
-                                            fileInputRef.current?.click()
-                                        }
-                                    />
-                                </ImageInput>
-                            </div>
-                        </div>
-                        <div className="sub-section">
-                            <div className="w-[303.19px]">
-                                <span className="sub-text">휴대폰 번호</span>
-                                <Input
-                                    placeholder="010-1234-5678"
-                                    onChange={(e) =>
-                                        handleApplyData(e.target.value, "phone")
-                                    }
-                                    value={applyData.phone.toString()}
-                                />
-                            </div>
-                            <div className="birth">
-                                <DropDown
-                                    hint="생년월일"
-                                    type="year"
-                                    value={birthData.year + "년"}
-                                    onChange={(e) =>
-                                        handleBirthData(e.target.value, "year")
-                                    }
-                                />
-                                <DropDown
-                                    type="month"
-                                    width="110px"
-                                    value={birthData.month + "월"}
-                                    onChange={(e) =>
-                                        handleBirthData(e.target.value, "month")
-                                    }
-                                />
-                                <DropDown
-                                    type="day"
-                                    width="110px"
-                                    value={birthData.day + "일"}
-                                    onChange={(e) =>
-                                        handleBirthData(e.target.value, "day")
-                                    }
-                                />
-                            </div>
-                            <div>
-                                <DropDown
-                                    hint="성별"
-                                    items={["남성", "여성"]}
-                                    value={
-                                        applyData.gender === true
-                                            ? "남성"
-                                            : "여성"
-                                    }
-                                    onChange={(e) =>
-                                        handleApplyData(
-                                            e.target.value === "남성",
-                                            "gender"
-                                        )
-                                    }
-                                />
-                            </div>
-                        </div>
-                        <PostalCode
-                            initialAddress={{
-                                address: applyData.address,
-                                detailAddress: applyData.detailAddress,
-                                zonecode: applyData.zip,
-                            }}
-                            onChange={handleAddressChange}
-                        />
-                    </div>
-                    <div>
-                        <Button title="제출하기" small onClick={onSubmit} />
-                    </div>
-                </Canary>
-            ) : (
-                <SuccessModal>
-                    <div className="title-text">인증 서류를 제출했습니다.</div>
-                    <div className="sub-text">
-                        자립준비청년 인증까지 영업일 기준 최대 10일 소요됩니다.
-                    </div>
-                    <Button
-                        title="닫기"
-                        small
-                        background="white"
-                        margin={false}
-                        onClick={onClick}
+          <span className="title-text">자립준비청년 인증하기</span>
+          <div className="input-section main-text">
+            <div className="sub-section">
+              <div className="w-[662.79px]">
+                <span className="sub-text">보호종료확인서 업로드</span>
+                <ImageInput>
+                  <div className="input-field">
+                    <Input
+                      type="file"
+                      onChange={handleImageChange}
+                      style={{ display: "none" }} // Hide the file input
+                      ref={fileInputRef} // Use a ref to interact with this input
                     />
-                </SuccessModal>
-            )}
-        </CanaryWrapper>
-    );
+                    <span>{image?.name || ""}</span>
+                  </div>
+                  <Button
+                    title="업로드"
+                    mainColor
+                    small
+                    onClick={() => fileInputRef.current?.click()}
+                  />
+                </ImageInput>
+              </div>
+            </div>
+            <div className="sub-section">
+              <div className="w-[303.19px]">
+                <span className="sub-text">휴대폰 번호</span>
+                <Input
+                  placeholder="010-1234-5678"
+                  onChange={(e) => handleApplyData(e.target.value, "phone")}
+                  value={applyData.phone.toString()}
+                />
+              </div>
+              <div className="birth">
+                <DropDown
+                  hint="생년월일"
+                  type="year"
+                  value={birthData.year + "년"}
+                  onChange={(e) => handleBirthData(e.target.value, "year")}
+                />
+                <DropDown
+                  type="month"
+                  width="110px"
+                  value={birthData.month + "월"}
+                  onChange={(e) => handleBirthData(e.target.value, "month")}
+                />
+                <DropDown
+                  type="day"
+                  width="110px"
+                  value={birthData.day + "일"}
+                  onChange={(e) => handleBirthData(e.target.value, "day")}
+                />
+              </div>
+              <div>
+                <DropDown
+                  hint="성별"
+                  items={["남성", "여성"]}
+                  value={applyData.gender === true ? "남성" : "여성"}
+                  onChange={(e) =>
+                    handleApplyData(e.target.value === "남성", "gender")
+                  }
+                />
+              </div>
+            </div>
+            <PostalCode
+              initialAddress={{
+                address: applyData.address,
+                detailAddress: applyData.detailAddress,
+                zonecode: applyData.zip,
+              }}
+              onChange={handleAddressChange}
+            />
+          </div>
+          <div>
+            <Button title="제출하기" small onClick={onSubmit} />
+          </div>
+        </Canary>
+      ) : (
+        <SuccessModal>
+          <div className="title-text">인증 서류를 제출했습니다.</div>
+          <div className="sub-text">
+            자립준비청년 인증까지 영업일 기준 최대 10일 소요됩니다.
+          </div>
+          <Button
+            title="닫기"
+            small
+            background="white"
+            margin={false}
+            onClick={onClick}
+          />
+        </SuccessModal>
+      )}
+    </CanaryWrapper>
+  );
 };
 
 export default CanaryModal;
 
 const SuccessModal = styled.div`
-    ${tw`
+  ${tw`
         w-[636px] h-[294px] rounded-[16px] bg-background flex flex-col justify-center items-center gap-[23px]
     `}
-    .title-text {
-        ${tw`
+  .title-text {
+    ${tw`
             text-bold-36 font-bold text-fontColor1
         `}
-    }
-    .sub-text {
-        ${tw`
+  }
+  .sub-text {
+    ${tw`
             text-medium-20 font-medium text-fontColor3
         `}
-    }
+  }
 `;
 
 const ImageInput = styled.div`
-    ${tw`
+  ${tw`
         flex gap-[20px]
     `}
-    .input-field {
-        ${tw`
+  .input-field {
+    ${tw`
             relative w-[662px]
         `}
-        span {
-            ${tw`
+    span {
+      ${tw`
         absolute left-0 top-0 w-[500px] h-full flex items-center ml-[21px]
         overflow-hidden whitespace-nowrap
     `}
-        }
     }
+  }
 `;
 
 const CanaryWrapper = styled.div`
-    ${tw`
+  ${tw`
         w-full h-full bg-[black] bg-opacity-50 
         fixed top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%]
         flex justify-center items-center
@@ -309,57 +305,57 @@ const CanaryWrapper = styled.div`
 `;
 
 const Canary = styled.div`
-    ${tw`
+  ${tw`
         w-[1222px] h-[660px] bg-background rounded-[16px]  flex flex-col 
         justify-center items-start p-[59px 57px] gap-[40px]
     `}
 
-    button {
-        ${tw`
+  button {
+    ${tw`
         h-[46px]
     `}
-    }
+  }
 
-    .input-section {
-        ${tw`
+  .input-section {
+    ${tw`
             flex flex-col gap-[36px]
         `}
-    }
+  }
 
-    .sub-section {
-        ${tw`
+  .sub-section {
+    ${tw`
             flex gap-[20px] items-end
         `}
-    }
+  }
 
-    .title-text {
-        ${tw`
+  .title-text {
+    ${tw`
             text-bold-36 font-bold text-fontColor1
         `}
-    }
+  }
 
-    .sub-text {
-        ${tw`
+  .sub-text {
+    ${tw`
             text-medium-20 font-medium text-fontColor3
         `}
-    }
+  }
 
-    .main-text {
-        ${tw`
+  .main-text {
+    ${tw`
             text-medium-20 font-medium text-fontColor1
         `}
-    }
+  }
 
-    .background {
-        ${tw`
+  .background {
+    ${tw`
             bg-white p-[0px 21px] rounded-full h-[46px]
             flex gap-[12px] items-center
         `}
-    }
+  }
 
-    .birth {
-        ${tw`
+  .birth {
+    ${tw`
             flex gap-[10px]
         `}
-    }
+  }
 `;
